865b28c801a31001bb2a8015e9faf670e656ed8c
gharras <med.gharras@gmail.com>
2019-10-07 22:01:17 +0100

    starting project of the DataBinding course

---
+++ app/src/main/java/pluralsight/com/tabiangifts/ViewProductFragment.java
@@ -0,0 +1,56 @@
+package pluralsight.com.tabiangifts;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import pluralsight.com.tabiangifts.databinding.FragmentViewProductBinding;
+import pluralsight.com.tabiangifts.models.Product;
+import pluralsight.com.tabiangifts.util.Products;
+
+
+/**
+ * Created by User on 2/6/2018.
+ */
+
+public class ViewProductFragment extends Fragment {
+
+    private static final String TAG = "ViewProductFragment";
+
+    // Data binding
+    FragmentViewProductBinding mBinding;
+    
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = FragmentViewProductBinding.inflate(inflater);
+        
+        Products products = new Products();
+        mBinding.setProduct(products.PRODUCTS[0]);
+    
+        mBinding.setQty(1);
+
+        return mBinding.getRoot();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
